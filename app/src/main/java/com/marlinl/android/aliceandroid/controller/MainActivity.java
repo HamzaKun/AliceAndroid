bb3ccbcb37309c76384a4a6a538806eef9948da8
Lin Ma <malin3369@gmail.com>
2016-01-17 23:34:47 +0800

    init

---
+++ app/src/main/java/com/marlinl/android/aliceandroid/controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.marlinl.android.aliceandroid.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MarlinL on 1/17/16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
